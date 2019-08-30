2183864250b91f645314ad59bc6057a1fd0766a5
sam-eng <sme388@nyu.edu>
2019-08-30 09:11:23 -0400

    update gitignore; clean up locations

--- vehicle_locations/jfk_locations.py
+++ vehicle_locations/jfk_locations.py
@@ -41,15 +41,8 @@ def main():
         locations_url = "http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=jfk&r=" + tag + "&t=0"
         locations_json = get_json_parsed(locations_url)
         if "vehicle" in locations_json:
-            print(locations_json["vehicle"])
-            #item title = locations_json["vehicle"]["id"]
-            #item lat = lat
-            #item long = lon
-            #description = route title?
-            #for vehicle in locations_json["vehicle"]:
             vehicles.append(vehicle_json_to_xml(locations_json["vehicle"], titles[title_counter]))
         title_counter = title_counter + 1
-        print(vehicles)
     xml_header = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\" xmlns:geo=\"http://www.w3.org/2003/01/geo/wgs84_pos#\">\n\t<channel>\n\t\t"
     xml_content = "<title></title>\n\t\t<link />\n\t\t<description />\n\t\t"
     xml_footer = "\n\t</channel>\n</rss>"
